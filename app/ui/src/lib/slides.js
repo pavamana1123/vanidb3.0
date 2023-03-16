59647f554a82e07109e81e0245aa414df9fb1500
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-03-16 12:53:55 +0530

    fitting complete

--- app/ui/src/lib/slides.js
+++ app/ui/src/lib/slides.js
@@ -5,20 +5,26 @@ const paginate = (text, className)=>{
     position: absolute;
     top: 0;
     z-index: -999;`
+    document.body.appendChild(d)
 
     var words = text.split(" ")
-    var start, end = 0
+    var start = 0
+    var end = 1
+    var slides = []
     
     while(end<=words.length){
-        d.textContent=words.slice(start,end)
+        d.textContent=words.slice(start,end).join(" ").trim()
         if(d.scrollHeight>d.clientHeight){
-            
+            end--
+            slides.push(words.slice(start,end).join(" ").trim())
+            start=end
+            end++
+            continue
         }
+        end++
     }
-
     d.remove()
-
-    
+    return slides    
 }
 
 export default paginate
--- app/ui/src/pages/slides/index.js
+++ app/ui/src/pages/slides/index.js
@@ -1,29 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import './index.css';
+import paginate from '../../lib/slides';
+import txt from './sample';
 
 function Slides(props) {
 
-  var Box = ()=>{
-    return <div style={{
-      border: "1px solid black",
-      height: "80vh",
-      width: "80vw",
-      padding: "2vw",
-      fontSize: "3.5vw",
-      overflow: "hidden",
-      textAlign: "center"
-    }} id="box">asd asd asd asd asdasdasdasd asdasdasda as da sda sd asd asd asd a asdkjas dajsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksd jkasdk jasdkjasd jasdjasdjas dasd asjd sd asjd asjdka sjdaksjd aksjdka sjsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksjd aksjdka sjsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksjd aksjdka sjsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksjd aksjdka sjsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksjd aksjdka sjsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksjd aksjdka sjsd lk sdkajs dkajsd jkasdk jasdkjasd jasdjasdjas dasd asjd asjdka sjdaksjd aksjdka sjdalksjdlaksjd lasjdlkj
-    </div>
-  }
+  var slides = paginate(txt, "box")
 
-  useEffect(()=>{
-    var myDiv = document.getElementById('box');
-    console.log(myDiv.scrollHeight, myDiv.clientHeight)
-  },[])
+  console.log(slides)
 
   return (
     <div className='slides'>
-      <Box/>
+      <div className='box'>{slides[1]}</div>
     </div>
   )
 
