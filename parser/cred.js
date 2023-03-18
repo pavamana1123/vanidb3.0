b397e21e238f7d5a10306eeaa924eacd65cfd77a
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-03-18 09:42:18 +0530

    ok

---
+++ parser/cred.js
@@ -0,0 +1,10 @@
+var cred = {
+    mysql: {
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "vanidb",
+    }
+}
+
+module.exports = cred
--- parser/index.js
+++ parser/index.js
@@ -1,7 +1,18 @@
+
+
 var HTMLParser = require('node-html-parser');
 
 const https = require('https');
-const url = 'https://vanisource.org/wiki/SB_9.18_Summary';
+const url = 'https://vanisource.org/wiki/SB_5.18.8';
+
+var cred = require("./cred.js")
+const DB = require("./db.js")
+
+cred.mysql.connectionLimit = 100
+cred.mysql.multipleStatements = true
+
+var mysql = require('mysql');
+var db = new DB(mysql.createPool(cred.mysql))
 
 function getHtml(url) {
     return new Promise(function(resolve, reject) {
@@ -19,83 +30,128 @@ function getHtml(url) {
     });
 }
 
-getHtml(url)
-  .then(function(data) {
-    const root = HTMLParser.parse(data)
+function parseHtml(data) {
+  const root = HTMLParser.parse(data)
 
-    const name = root.querySelector("#firstHeading").textContent.trim()
-    const book = name.split(" ")[0]
-    const parts = name.split(" ")[1].split(".")
-    var canto = null
-    var chapter = null
-    var text = null
-    if(name.includes("Summary")){
+  const name = root.querySelector("#firstHeading").textContent.trim()
+  const book = name.split(" ")[0]
+  const parts = name.split(" ")[1].split(".")
+  var canto = null
+  var chapter = null
+  var text = null
+  if(name.includes("Summary")){
+    canto = parts[0]
+    chapter = parts[1].split(" ")[0]
+    text = "0"
+  }else{
+    if(parts.length==3){
       canto = parts[0]
-      chapter = parts[1].split(" ")[0]
-      text = "0"
-    }else{
-      if(parts.length==3){
-        canto = parts[0]
-        chapter = parts[1]
-        text = parts[2]
-      }else if (parts.length==2){
-        chapter = parts[0]
-        text = parts[1]
-      }
+      chapter = parts[1]
+      text = parts[2]
+    }else if (parts.length==2){
+      chapter = parts[0]
+      text = parts[1]
     }
+  }
 
-    const verseCount = (()=>{
-      const x = text.split("-")
-      if(x.length==1){
-        return 1
-      }
-      return parseInt(x[1].replace(/\D/g,''))-parseInt(x[0].replace(/\D/g,''))+1
-    })()
+  const verseCount = (()=>{
+    const x = text.split("-")
+    if(x.length==1){
+      return 1
+    }
+    return parseInt(x[1].replace(/\D/g,''))-parseInt(x[0].replace(/\D/g,''))+1
+  })()
 
-    var versesNode = (()=>{
-      var x = root.querySelector(".verse")
-      if(!!x){
-        return x.getElementsByTagName("dl")
-      }
-      return []
+  var versesNode = (()=>{
+    var x = root.querySelector(".verse")
+    if(!!x){
+      return x.getElementsByTagName("dl")
+    }
+    return []
+    
+  })()
+  const verses = versesNode.map(n=>{return n.textContent.trim()})
+  const proseFlags = verses.map((v)=>{return v.split("\n").length>5})
+
+  const synonyms = (()=>{
+    var x = root.querySelector(".synonyms")
+    if(!!x){
+      return x.textContent.trim()
+    }
+    return null
+  })()
+
+  const purport = (()=>{
+    var x = root.querySelector(".purport")
+    if(!!x){
+      return x.textContent.trim()
+    }
+    return null
+  })()
+
+  const links = (()=>{
+    var x = root.querySelector(".mw-parser-output").getElementsByTagName("a")
+    return [x[x.length-1], x[x.length-3]]
+  })()
+
+  const next = links[0].getAttribute("title")
+  const prev = links[1].getAttribute("title")
+
+  const nextLink = links[0].getAttribute("href")
+  const prevLink = links[1].getAttribute("href")
+
+  const chapterName = (()=>{
+    var x = root.querySelector(".mw-parser-output").getElementsByTagName("b")[0].getElementsByTagName("a")
+    return x[2].textContent.split(":")[1].trim()
+  })()
+
+  return {name, book, canto, chapter, text, verses, proseFlags, synonyms, purport, next, prev, nextLink, prevLink, chapterName}
+}
+
+getHtml(url)
+  .then((data)=>{
+    var content = parseHtml(data)
+    content.verses.map((v, i)=>{
       
-    })()
-    const verses = versesNode.map(n=>{return n.textContent.trim()})
-    const proseFlags = verses.map((v)=>{return v.split("\n").length>5})
+      var query = `INSERT INTO texts
+      (name,
+      book,
+      canto,
+      chapter,
+      text,
+      verse,
+      synonyms,
+      translation,
+      purport,
+      next,
+      prev,
+      nextLink,
+      prevLink)
+      VALUES
+      ("${verse.name.replaceAll('"','\\"')}",
+      "${verse.book.replaceAll('"','\\"')}",
+      "${verse.canto.replaceAll('"','\\"')}",
+      "${verse.chapter.replaceAll('"','\\"')}",
+      "${verse.text.replaceAll('"','\\"')}",
+      "${verse.verse.replaceAll('"','\\"')}",
+      "${verse.synonyms.replaceAll('"','\\"')}",
+      "${verse.translation.replaceAll('"','\\"')}",
+      "${verse.purport.replaceAll('"','\\"')}",
+      "${verse.next.replaceAll('"','\\"')}",
+      "${verse.prev.replaceAll('"','\\"')}",
+      "${verse.nextLink.replaceAll('"','\\"')}",
+      "${verse.prevLink.replaceAll('"','\\"')}"
+      ;
+      `
+      console.log(query)
+      db.execQuery(query).then((err, res)=>{
+        console.log(err, res)
+      }).catch((err)=>{
+        console.log(err)
+      })
+    })
 
-    const synonyms = (()=>{
-      var x = root.querySelector(".synonyms")
-      if(!!x){
-        return x.textContent.trim()
-      }
-      return null
-    })()
 
-    const purport = (()=>{
-      var x = root.querySelector(".purport")
-      if(!!x){
-        return x.textContent.trim()
-      }
-      return null
-    })()
-
-    const links = (()=>{
-      var x = root.querySelector(".mw-parser-output").getElementsByTagName("a")
-      return [x[x.length-1], x[x.length-3]]
-    })()
-
-    const next = links[0].getAttribute("title")
-    const prev = links[1].getAttribute("title")
-
-    const nextLink = links[0].getAttribute("href")
-    const prevLink = links[1].getAttribute("href")
-
-    const chapterName = (()=>{
-      var x = root.querySelector(".mw-parser-output").getElementsByTagName("b")[0].getElementsByTagName("a")
-      return x[2].textContent.split(":")[1].trim()
-    })()
-
-    console.log(name, book, canto, chapter, text, verses, synonyms, purport, next, prev, nextLink, prevLink, chapterName)
   })
   .catch(function(err) {
     console.log(err)
