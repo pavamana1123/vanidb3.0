90b25e3fd2e10bbde74c9d39376b6834b3ea275e
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-04-10 15:27:54 +0530

    changes

--- parser/index.js
+++ parser/index.js
@@ -1,7 +1,6 @@
 var HTMLParser = require('node-html-parser');
 
 const https = require('https');
-var url = 'https://vanisource.org/wiki/SB_12.13.23';
 
 var cred = require("./cred.js")
 const DB = require("./db.js")
@@ -12,6 +11,15 @@ cred.mysql.multipleStatements = true
 var mysql = require('mysql');
 var db = new DB(mysql.createPool(cred.mysql))
 
+const books = {
+  sb: {
+    startLink: "https://vanisource.org/wiki/SB_12.13.23",
+    endLink: "https://vanisource.org/wiki/SB_1.1.1"
+  }
+}
+
+var book = "sb"
+var url = books[book].startLink
 var overwrite = true
 
 String.prototype.escape = function(){
@@ -35,129 +43,141 @@ function getHtml(url) {
 }
 
 function parseHtml(data) {
-  const root = HTMLParser.parse(data)
 
-  const name = root.querySelector("#firstHeading").textContent.trim()
-  const book = name.split(" ")[0]
-  const parts = name.split(" ")[1].split(".")
-  var canto = null
-  var chapter = null
-  var texts = []
-  var text = ""
-  var group = false
-  var isSummary = false
-  if(name.includes("Summary")){
-    canto = parts[0]
-    chapter = parts[1].split(" ")[0]
-    texts = ["Summary"]
-    isSummary = true
-  }else{
-    if(parts.length==3){
+  return new Promise(function(resolve, reject) {
+    const root = HTMLParser.parse(data)
+
+    const name = root.querySelector("#firstHeading").textContent.trim()
+    const book = name.split(" ")[0]
+    const parts = name.split(" ")[1].split(".")
+    var canto = null
+    var chapter = null
+    var texts = []
+    var text = ""
+    var group = false
+    var isSummary = false
+    if(name.includes("Summary")){
       canto = parts[0]
-      chapter = parts[1]
-      text = parts[2]
-    }else if (parts.length==2){
-      chapter = parts[0]
-      text = parts[1]
+      chapter = parts[1].split(" ")[0]
+      texts = ["Summary"]
+      isSummary = true
+    }else{
+      if(parts.length==3){
+        canto = parts[0]
+        chapter = parts[1]
+        text = parts[2]
+      }else if (parts.length==2){
+        chapter = parts[0]
+        text = parts[1]
+      }
+      texts = (()=>{
+        const hasAlpha = /[a-zA-Z]/.test(text)
+        const alpha = hasAlpha?text.match(/[a-zA-Z]+/g)[0]:""
+        const parts = text.split("-")
+        if(parts.length==1){
+          return parts
+        }
+        group = true
+        var texts = []
+        for (var i = parseInt(parts[0].replaceAll(alpha,"")); i <= parseInt(parts[1].replaceAll(alpha,"")); i++) {
+            texts.push(i+alpha)
+        }
+        return texts
+      })()
     }
-    texts = (()=>{
-      const hasAlpha = /[a-zA-Z]/.test(text)
-      const alpha = hasAlpha?text.match(/[a-zA-Z]+/g)[0]:""
-      const parts = text.split("-")
-      if(parts.length==1){
-        return parts
+
+    var versesNode = (()=>{
+      var x = root.querySelector(".verse")
+      if(!!x){
+        return x.getElementsByTagName("dl")
       }
-      group = true
-      var texts = []
-      for (var i = parseInt(parts[0].replaceAll(alpha,"")); i <= parseInt(parts[1].replaceAll(alpha,"")); i++) {
-          texts.push(i+alpha)
-      }
-      return texts
+      return []
+      
     })()
-  }
+    const verses = isSummary?[""]:versesNode.map(n=>{return Buffer.from(n.textContent.trim()).toString('base64')})
+    const proseFlags = versesNode.map((n)=>{return n.textContent.trim().split("\n").length<4})
+    group = verses.length>1
 
-  var versesNode = (()=>{
-    var x = root.querySelector(".verse")
-    if(!!x){
-      return x.getElementsByTagName("dl")
-    }
-    return []
-    
-  })()
-  const verses = isSummary?[""]:versesNode.map(n=>{return Buffer.from(n.textContent.trim()).toString('base64')})
-  const proseFlags = versesNode.map((n)=>{return n.textContent.trim().split("\n").length<4})
-  group = verses.length>1
-
-  const synonyms = (()=>{
-    var x = root.querySelector(".synonyms")
-    if(!!x){
-      return Buffer.from(x.textContent.trim()).toString('base64')
-    }
-    return isSummary?"":null
-  })()
-
-  const translation = (()=>{
-    var x = root.querySelector(".translation")
-    if(!!x){
-      return Buffer.from(x.textContent.trim()).toString('base64')
-    }
-    return isSummary?"":null
-  })()
-
-  const purport = (()=>{
-    var purport = []
-    var paragraphs = root.querySelector(".purport")
-    if(!!paragraphs){
-      for(var i=0; i<paragraphs.childNodes.length; i++){
-        if(!paragraphs.childNodes[i].textContent.trim()){
-          continue
-        }
-        purport.push({
-          text: paragraphs.childNodes[i].textContent.trim()
-        })
-        if(paragraphs.childNodes[i].rawTagName=="dl"){
-          purport[purport.length-1].isRef=true
-        }
+    const synonyms = (()=>{
+      var x = root.querySelector(".synonyms")
+      if(!!x){
+        return Buffer.from(x.textContent.trim()).toString('base64')
       }
-      return Buffer.from(JSON.stringify(purport)).toString('base64')
+      return isSummary?"":null
+    })()
+
+    const translation = (()=>{
+      var x = root.querySelector(".translation")
+      if(!!x){
+        return Buffer.from(x.textContent.trim()).toString('base64')
+      }
+      return isSummary?"":null
+    })()
+
+    const purport = (()=>{
+      var purport = []
+      var paragraphs = root.querySelector(".purport")
+      if(!!paragraphs){
+        for(var i=0; i<paragraphs.childNodes.length; i++){
+          if(!paragraphs.childNodes[i].textContent.trim()){
+            continue
+          }
+          purport.push({
+            text: paragraphs.childNodes[i].textContent.trim()
+          })
+          if(paragraphs.childNodes[i].rawTagName=="dl"){
+            purport[purport.length-1].isRef=true
+          }
+        }
+        return Buffer.from(JSON.stringify(purport)).toString('base64')
+      }
+      return null
+    })()
+
+    const links = (()=>{
+      var x = root.querySelector(".mw-parser-output").getElementsByTagName("a")
+      return [x[x.length-1], x[x.length-3]]
+    })()
+
+    var next = links[0].getAttribute("title")
+    var prev = links[1].getAttribute("title")
+
+    var nextLink = links[0].getAttribute("href")
+    var prevLink = links[1].getAttribute("href")
+
+    if(url==books[book].startLink){
+      next=null
+      nextLink=null
     }
-    return null
-  })()
+    if(url==books[book].endLink){
+      prev=null
+      prevLink=null
+    }
 
-  const links = (()=>{
-    var x = root.querySelector(".mw-parser-output").getElementsByTagName("a")
-    return [x[x.length-1], x[x.length-3]]
-  })()
+    const chapterName = (()=>{
+      var x = root.querySelector(".mw-parser-output").getElementsByTagName("b")[0].getElementsByTagName("a")
+      return x[x.length-1].textContent.split(":")[1].trim()
+    })()
 
-  const next = links[0].getAttribute("title")
-  const prev = links[1].getAttribute("title")
-
-  const nextLink = links[0].getAttribute("href")
-  const prevLink = links[1].getAttribute("href")
-
-  const chapterName = (()=>{
-    var x = root.querySelector(".mw-parser-output").getElementsByTagName("b")[0].getElementsByTagName("a")
-    return x[x.length-1].textContent.split(":")[1].trim()
-  })()
-
-  const parsedContent =  {name, book, canto, chapter, texts, verses, proseFlags, group, synonyms, translation ,purport, next, prev, nextLink, prevLink, chapterName, isSummary}
-  if(!isSummary && (verses.length!=texts.length)){
-    throw new Error("Mismatch len:", parsedContent.name)
-  }
-  return parsedContent
+    const parsedContent =  {name, book, canto, chapter, texts, verses, proseFlags, group, synonyms, translation ,purport, next, prev, nextLink, prevLink, chapterName, isSummary}
+    if(!isSummary && (verses.length!=texts.length)){
+      throw new Error("Mismatch len:", parsedContent.name)
+    }
+    resolve(parsedContent)
+  });
 }
 
 function log(l){
-  console.log(`${parsedContent.name}> ${l}`)
+  console.log(`  ${parsedContent.name}> ${l}`)
 }
 
 var parsedContent
+var start = true
 
-console.log("starting parser")
-  console.log("waiting to get html")
+function storeVerse(){
   getHtml(url)
-  .then((data)=>{
-    console.log("waiting to parse html")
+  .then(parseHtml)
+  .then((parsedContent)=>{
     parsedContent = parseHtml(data)
     log("parsed html")
     parsedContent.verses.map((v, i)=>{
@@ -178,6 +198,7 @@ console.log("starting parser")
       \`prevLink\`,
       \`isProse\`,
       \`isGroup\`,
+      \`ord\`,
       \`isSummary\`
       )
       VALUES
@@ -197,6 +218,7 @@ console.log("starting parser")
       "${parsedContent.prevLink}",
       ${!!parsedContent.proseFlags[i]},
       ${!!parsedContent.group},
+      ${Date.now()},
       ${!!parsedContent.isSummary}
       )
       
@@ -217,26 +239,36 @@ console.log("starting parser")
       prevLink="${parsedContent.prevLink}",
       isProse=${!!parsedContent.proseFlags[i]},
       isGroup=${!!parsedContent.group},
+      ord=${Date.now()},
       isSummary=${!!parsedContent.isSummary}
       `:""}
       ;
       `
       log("writing data")
-      db.execQuery(query).then((err, res)=>{
+      db.execQuery(query).then((res, err)=>{
         if(err){
-          log(err)
+          console.log(err)
+          log(`database error ${err}`)
+        }else{
+          url = `https://vanisource.org${start?parsedContent.nextLink:parsedContent.prevLink}`
+          start=false
+          log("written to database")
+          storeVerse()
         }
-        url = parsedContent.prevLink
-        log("written to data base")
       }).catch((err)=>{
         console.log(err)
       })
     })
   })
+  .then
   .catch(function(err) {
+    console.log(url, parsedContent.prevLink, parsedContent.nextLink)
     log(err)
   });
-  console.log("step complete")
+}
+
+storeVerse()
+
 
 
 
