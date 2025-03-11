7aa16198159df6c83408a5df86806bca7626269d
Pavana Prana Dasa <pavamana1123@gmail.com>
2025-03-11 22:00:13 +0530

    asdasd

--- parser/sb/book/build.js
+++ parser/sb/book/build.js
@@ -1,10 +1,12 @@
-const newPage = () => {
+const newPage = id => {
     var page = document.createElement('div')
     page.className = 'page'
+    page.id = id
     document.body.appendChild(page)
+
+    var pageBreak = document.createElement('div')
+    pageBreak.className = 'page-break'
+    document.body.appendChild(pageBreak)
 }
 
-newPage()
-newPage()
-newPage()
-newPage()
+newPage(`1`)
